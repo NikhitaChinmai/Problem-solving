ae0c3471561bbc564a105edb95c90a61a4c545d1
NikhitaChinmai <147228315+NikhitaChinmai@users.noreply.github.com>
2025-01-24 09:31:55 +0530

    Create missing.c

---
+++ missing.c
@@ -0,0 +1,19 @@
+//missiing value in range n to n-1
+#include<stdio.h>
+int findm(int a[],int size){
+    int n=size-1;
+    int totalsum=(n*(n+1))/2;
+    int asum=0;
+    for(int i=0;i<n-1;i++){
+        asum+=a[i];
+    }
+    return totalsum - asum;
+}
+int main()
+{
+   int a[]={1,2,3,4,6,7,8};
+   int n=8;
+   int missingnumber=findm(a,n);
+   printf("The missing number is :%d\n",missingnumber);
+   return 0;
+}
